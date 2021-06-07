7e06f82052eed54be478d45e525f2ac6ed984716
Sanguinee <>
2021-06-07 07:59:15 +0000

    Did the project

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,6 +11,7 @@ let candidateAnswer;
 let questions;
 let correctAnswers;
 let candidateAnswers;
+let grade;
 
 
 function askForName() {
@@ -21,22 +22,35 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer
-  question = "Who was the first American woman in space? ";
-  candidateAnswer = input.question(question);
-  correctAnswer = "Sally Ride";
+  questions = ["\n1) Who was the first American woman in space? ", "\n2) True or false: 5 kilometer == 5000 meters? ", "\n3) (5 + 3)/2 * 10 = ? ", "\n4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "\n5) What is the minimum crew size for the ISS? "];
+  correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+
+  grade = 0;
+  for(i = 0; i < 5; i++){
+    console.log(questions[i]);
+    candidateAnswer = input.question(question);
+    if(candidateAnswer.toUpperCase() === correctAnswers[i].toUpperCase()){
+      grade++;
+    }
+    //console.log("debug: " + questions[i] + " " + questions[i].toUpperCase() + " " + correctAnswers[i] + " " + correctAnswers[i].toUpperCase())
+    console.log("Correct answer: " + correctAnswers[i]);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  let grade;
-  if (candidateAnswer === correctAnswer){
-    grade = "correct";
-  } else {
-    grade = "incorrect";
-  }
   
-  console.log("Your answer was " + grade + "!");
+  let finalgrade;
+  let status;
+  finalgrade = grade/5*100;
+  console.log("\n>>> Overall Grade: " + finalgrade + "% (" + grade + " of 5 responses correct) <<<");
+  if(grade >= 4){
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+  console.log(">>> Status: " + status + " <<<");
 }
 
 function runProgram() {
